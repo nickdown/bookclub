b33ccd402397bdce2a7127113fab6e3f87060881
Nick Down <nick@trafficstamp.com>
2018-08-12 20:17:01 -0400

    add a bunch of routes

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,11 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/users', 'UserController@index')->name('users.index');
 Route::get('/users/{user}', 'UserController@show')->name('users.show');
 
+Route::get('/profile/edit', 'UserController@edit')->name('users.edit');
+Route::patch('/profile', 'UserController@update')->name('users.update');
+
 Route::post('/user/books', 'BookUserController@store');
 Route::patch('/user/books/{book}', 'BookUserController@update');
+Route::patch('/user', 'UserController@update');
+Route::delete('/books/{book}/remove', 'BookUserController@destroy');
 Route::resource('/books', 'BookController');
