cce52cef40000ddf24ce15cbe20f43deb128cc4d
Nick Down <nick@relayclick.com>
2018-08-16 13:35:36 -0400

    update book item view

--- resources/views/books/partials/book-item.blade.php
+++ resources/views/books/partials/book-item.blade.php
@@ -1,11 +1,17 @@
-<div class="card text-center m-2 p-4" style="width: 16rem;">
-    @include('books.partials.book-image')
-    <div class="card-body">
-        <h5 class="card-title"><a href="{{ $book->url() }}">{{ $book->title }}</a></h5>
-        <div class="card-text">
-            <strong>Author:</strong> {{ $book->author }}
-            <br>
-            @include('partials.stars', ['rating' => $book->rating])
+<div class="card text-center m-2" style="width: 16rem;">
+    <div class="px-4 pt-4">
+        @include('books.partials.book-image')
+    </div>
+    <div class="card-body d-flex flex-column justify-content-between" style="min-height: 15rem;">
+        <a href="{{ $book->url() }}">{{ $book->title }}</a>
+        <span><strong>Author:</strong> {{ $book->author }}</span>
+        @include('partials.stars', ['rating' => $book->rating])
+        <div>
+            @if(auth()->user()->owns($book))
+                In Your Library <i class="fas fa-check"></i>
+            @else
+                @include('partials.add-book-to-library')
+            @endif
         </div>
     </div>
 </div>
